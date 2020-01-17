7d2d492ce40b7f9289b9fb1454a36385e9fbb9db
Luis PM <luisp237@gmail.com>
2020-01-17 03:00:27 -0500

    add axios get() then() catch()

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/azatecas')
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => {
+    console.log(`this is Error from`, error);
+  })
+  
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
